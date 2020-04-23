ee7eddb9ba0066be257beda355cf40c67edd9f7f
QWYNG <ikusawasi@gmail.com>
2020-04-23 20:15:26 +0900

    無駄なstack拡張snipetを削除

--- lib/green_day/snippet_builder.rb
+++ lib/green_day/snippet_builder.rb
@@ -1,16 +1,14 @@
 # frozen_string_literal: true
 
 module SnippetBuilder
-  INCREASE_STACK_SIZE_SNIPPET =
-    "ENV[Z = 'RUBY_THREAD_VM_STACK_SIZE'] || exec( { Z => '50000000' },'ruby',$0)"
-  MULTIPLE_LINE_INPUT_SNIPPET = 'readlines.map(&:chomp!).map { |e| e.split.map(&:to_i) }'
-  ARRAY_INPUT_SNIPPET = 'gets.split.map(&:to_i)'
+  ARRAY_INPUT_SNIPPET = 'n = gets.split.map(&:to_i)'
+  MULTIPLE_LINE_INPUT_SNIPPET = 'array = readlines.map(&:chomp!).map { |e| e.split.map(&:to_i) }'
 
   module_function
 
   def build
-    [INCREASE_STACK_SIZE_SNIPPET, MULTIPLE_LINE_INPUT_SNIPPET, ARRAY_INPUT_SNIPPET]
-      .map { |snippet| '# ' + snippet }
+    constants
+      .map { |snippet| '# ' + eval(snippet.to_s) }
       .join("\n") + "\n"
   end
 end
--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -40,9 +40,8 @@ RSpec.describe GreenDay::Cli do
     it 'write snippet code' do
       expect(File.read('abc150/A.rb')).to eq(
         <<~SNIPPET
-          # ENV[Z = 'RUBY_THREAD_VM_STACK_SIZE'] || exec( { Z => '50000000' },'ruby',$0)
-          # readlines.map(&:chomp!).map { |e| e.split.map(&:to_i) }
-          # gets.split.map(&:to_i)
+          # array = readlines.map(&:chomp!).map { |e| e.split.map(&:to_i) }
+          # n = gets.split.map(&:to_i)
         SNIPPET
       )
     end
